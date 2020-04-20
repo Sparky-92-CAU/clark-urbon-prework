7312f481129b77039743de63a189daa5fa39da0c
Sparky <urbonc@gmail.com>
2020-04-20 19:07:26 -0400

    edits to game.js

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,20 +1,63 @@
+
+//game object
 const game = {
     guesses: 10,
     wins: 0,
-    lettersUsed: ['a','b','c','d'],
-    word: 'guess'
+    lettersUsed: [],
+    word: '',
+    wordBank: ['apple','banana','grape','orange','kjahskfjhaskdjfhak'],
+    
+    resetBoard(){
 
-    setBoard(){
+        //Get html element references 
         const guesses = document.querySelector('#guesses');
         const used = document.querySelector('#used');
         const wins = document.querySelector('#wins');
-
-        guesses.innerText = `Guesses Left: ${this.guesses}`;
+        const wordDisplayed = document.querySelector('#word');
+       
+        //set html elements
+       guesses.innerText = `Guesses Left: ${this.guesses}`;
        used.innerText = `Letters Used: ${this.lettersUsed}`;
        wins.innerText = `Number of Wins: ${this.wins}`;
+       
+       //sets word to random word from wordbank
+       this.word = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
+
+       //build placeholder string
+       let wordPlaceholder = '';
+       for(let i =0;i<this.word.length;i++){
+           wordPlaceholder+='_ ';
+       };
+       // set placeholder
+       wordDisplayed.innerText = wordPlaceholder;
+      
+       console.log(this.word);
+    },
+
+    updateBoard(event){
+
+        const key = event.key;
+    if (game.word.includes(key)) {
+        console.log(`${key}`);
+        
     }
+    else {
+        console.log('not found');
+    }
+        
+
+    }
+
+   
+
+
+    
     
   };
 
-  game.setBoard();
+  game.resetBoard();
+
+document.addEventListener('keyup', game.updateBoard);
+ 
+  
 
